19ff43ce6aeed9c6d4628551bf66637c8b79e0e4
Voyager2718 <voyager2718@gmail.com>
2015-09-19 21:43:02 +0200

    TP1 AP2

--- University/TPs/COO/TP1/Dungeon/src/NormalRoom.java
+++ University/TPs/COO/TP1/Dungeon/src/NormalRoom.java
@@ -1,81 +1,36 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class NormalRoom implements Room {
-	protected Map<String, Room> neighbours = new HashMap<String, Room>();
-	protected String type;
-	protected String condition;
-
-	/**
-	 * Constructor
-	 * 
-	 * @param type
-	 * @param condition
-	 */
-	public NormalRoom() {
-		this.type = "normal room";
-		this.condition = "normal";
+public class NormalRoom extends Room {
+	public NormalRoom(String type) {
+		super(type);
 	}
 
-	public String getType() {
-		return this.type;
+	public void addRoom(String direction, Room room) {
+		if (!this.directionAvailable(direction)) {
+			System.out.println("I don't know what you mean.");
+			return;
+		}
+		if (this.rooms.containsKey(direction)) {
+			System.out.println("Already have a room there.");
+			return;
+		}
+		this.rooms.put(direction, room);
 	}
 
-	public String getCondition() {
-		return this.condition;
-	}
-
-	/**
-	 * Add next room to the map.
-	 * 
-	 * @param room
-	 *            A room.
-	 * @param direction
-	 *            The direction to place the room.
-	 */
-	@Override
-	public void addNextRoom(Room room, String direction) {
-		this.neighbours.put(direction, room);
-	}
-
-	/**
-	 * Get next room with the direction.
-	 * 
-	 * @param direction
-	 *            Get next room from the direction entered. If the direction
-	 *            doesn't exist, return null.
-	 */
-	@Override
-	public Room getNextRoom(String direction) {
-		if (!isPossibleDirection(direction))
-			return null;
-		return this.neighbours.get(direction);
-	}
-
-	/**
-	 * Check if there's a room from the direction entered.
-	 * 
-	 * @param direction
-	 *            The direction needs to be checked.
-	 */
-	@Override
 	public boolean isPossibleDirection(String direction) {
-		return this.neighbours.containsKey(direction);
+		return this.rooms.containsKey(direction);
 	}
 
-	/**
-	 * Try to go to next room. If it's impossible to go, return null, else
-	 * return the next room.
-	 * 
-	 * @param command
-	 *            A command begin with "Go" end with a direction(Such as
-	 *            "Go north", "Go south").
-	 */
-	@Override
-	public Room go(String command) throws CantGoException {
-		if (isPossibleDirection(command.substring(3))) {
-			return getNextRoom(command.substring(3));
-		} else
-			throw new CantGoException("You can't go there.");
+	public Room getNextRoom(String direction) throws CantGoException {
+		if (!this.directionAvailable(direction)) {
+			System.out.println("I don't know what you mean.");
+			return null;
+		}
+		if (!this.rooms.containsKey(direction))
+			throw new CantGoException("Can't go " + direction);
+		return this.rooms.get(direction);
 	}
 }
--- University/TPs/COO/TP1/Dungeon/src/main.java
+++ University/TPs/COO/TP1/Dungeon/src/main.java
@@ -1,6 +1,5 @@
 public class main {
 	public static void main(String args[]) {
-		Dungeon play = new Dungeon();
-		play.start();
+		
 	}
 }
