44ea770a075da9e91fa125c52654591028ee223a
Voyager2718 <voyager2718@gmail.com>
2015-10-01 17:20:28 +0200

    COO TP1

---
+++ University/TPs/COO/TP1/Dungeon/src/Treasure.java
@@ -0,0 +1,17 @@
+public class Treasure implements Equipement {
+	private String name;
+	private int value;
+
+	public Treasure(String name, int value) {
+		this.name = name;
+		this.value = value;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getValue() {
+		return value;
+	}
+}
