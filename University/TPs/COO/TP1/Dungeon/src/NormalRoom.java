cd740a33a18cf238abad71bd8cc5a57eb913e293
Voyager2718 <voyager2718@gmail.com>
2015-10-01 19:42:19 +0200

    增加特性

--- University/TPs/COO/TP1/Dungeon/src/NormalRoom.java
+++ University/TPs/COO/TP1/Dungeon/src/NormalRoom.java
@@ -1,5 +1,30 @@
 public class NormalRoom extends Room {
+	protected int position;
+	protected boolean found;
+
 	public NormalRoom() {
 		super("Normal", "This is a normal(intersection) room.");
+		this.position = (int) Math.random() * 10;
+		if (this.position > 90)
+			this.found = true;
+		else
+			this.found = false;
+	}
+
+	public String getPosition() {
+		if (this.position <= 3)
+			return "table";
+		if (this.position <= 100)
+			return "picture";
+		else
+			return "normal";
+	}
+
+	public void found() {
+		this.found = true;
+	}
+
+	public boolean getFound() {
+		return this.found;
 	}
 }
