74d33cc0b06072b84874fdbc1ed5ae5b961c1e14
Voyager2718 <Voyager2718@gmail.com>
2015-10-20 21:46:45 +0200

    Good version of TP5 exo5

--- University/TPs/PDS/exo5.TP5.c
+++ University/TPs/PDS/exo5.TP5.c
@@ -9,29 +9,28 @@ int testFunc(int i){
 	return i;
 }
 
-void resultF(int resultOfFunction, int *result){
+void changeResult(int resultOfFunction, int *result){
 	if( resultOfFunction != 0 ){
 		*result = *result & 1;
 	}else{
 		*result = 0;
 	}
-}//Change the value of result.
+}
 
 int multif(func_t f[], int args[], int n){
 	int i, result = 1, child;
 	pid_t pid;
+	
 	for( i = 0 ; i < n ; i++ ){
 		switch(fork()){
 			case -1:
 				perror("Error.");
 				exit(EXIT_FAILURE);
 			case 0: 
-				resultF(f[i](args[i]),&result);
-				exit(result);
+				exit(f[i](args[i]));
 			default:
 				wait(&child);
-				int result = WEXITSTATUS(child);
-				break;
+				changeResult(WEXITSTATUS(child),&result);
 		}
 	}
 	return result;
@@ -39,8 +38,7 @@ int multif(func_t f[], int args[], int n){
 
 int main(void){
 	func_t f[3] = {testFunc, testFunc, testFunc};
+	int args[3] = {2,1,3};
 
-	int a[3] = {2,1,0};
-
-	printf("%d\n",multif(f,a,3));
+	printf("%d\n",multif(f,args,3));
 }
