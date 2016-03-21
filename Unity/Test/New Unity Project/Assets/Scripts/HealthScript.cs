6e123abf977f197296d06cb2eed5fa9e93972fd5
Voyager2718 <voyager2718@gmail.com>
2016-03-21 23:46:58 +0100

    Added an Unity 3D test project.

---
+++ Unity/Test/New Unity Project/Assets/Scripts/HealthScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthScript : MonoBehaviour
+{
+    public int hp = 5;
+
+    public bool isEnemy = true;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        BulletScript bullet = collider.gameObject.GetComponent<BulletScript>();
+
+        if (bullet != null && bullet.isEnemyShot != isEnemy)
+        {
+            hp -= bullet.damage;
+            Destroy(bullet.gameObject);
+            if (hp <= 0)
+                Destroy(gameObject);
+        }
+    }
+}
